f47ff34205e125a7c835d9026afb7ad98e4ece97
Virvon <dan130205@gmail.com>
2022-09-29 14:12:42 +0300

    Creating variables

---
+++ Creating varibles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creating_varibles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countCow; // кол-во коров на ферме
+            string playerNickname; // имя игрока
+            bool isRed; // проверка красного цвета
+            int enemySpeed; //скорость врага
+            bool isPress; // проверка нажатия 
+            int marketMoneyCount; // ко-во денег в магазине
+            string itemName; // название предмета
+            string itemRarity;// редкость предмета
+            int wheelDiameter; // диаметр колеса у автомобиля
+            string password; // пароль 
+        }
+    }
+}
