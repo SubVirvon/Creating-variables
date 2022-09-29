7f24e6c7d323425a264b1084ffd6cbc2e8ae1b10
Virvon <dan130205@gmail.com>
2022-09-29 19:32:56 +0300

    Update Program.cs

--- Creating varibles/Program.cs
+++ Creating varibles/Program.cs
@@ -10,16 +10,16 @@ namespace Creating_varibles
     {
         static void Main(string[] args)
         {
-            int countCow; // кол-во коров на ферме
-            string playerNickname; // имя игрока
-            bool isRed; // проверка красного цвета
-            int enemySpeed; //скорость врага
-            bool isPress; // проверка нажатия 
-            int marketMoneyCount; // ко-во денег в магазине
-            string itemName; // название предмета
-            string itemRarity;// редкость предмета
-            int wheelDiameter; // диаметр колеса у автомобиля
-            string password; // пароль 
+            int userMoney;
+            bool isOpened;
+            string userNickname;
+            long playersCount;
+            ushort cubeVolume;
+            char symbol;
+            byte carsCount;
+            uint enemySpeed;
+            ulong allMoney;
+            float gameResult;
         }
     }
 }
